771c2cf70f58fcb364c93c6219f3f44fece0e331
vane000 <vanessa.suazo.r4@gmail.com>
2023-11-13 16:30:04 -0300

    solo funciones basica y la formula de la profe

--- proyecto2 (copia).cpp
+++ proyecto2 (copia).cpp
@@ -22,11 +22,24 @@ int reconeG(string cadena){
     return letraG;
 }
 
+double formulaGenoma(double C, double G, double largo){
+    return (C+G)/largo;
+}
+
 int main(){
+
+    
     string prueba= "A,C,G,A,T,C,A,G,C,G,G,T";
+    int largo = prueba.length();
+    cout << largo<<endl;
     int a=reconeC(prueba);
     cout << a <<endl;
     int b=reconeG(prueba);
     cout << b <<endl;
+    double umbral=formulaGenoma(a,b,largo);
+    cout<< umbral<< endl;
+ 
+
+
     return 0;
 }
